62123679d0114ce8d5e3d87186de26be79bd3b11
Danilo Valério da Silva <danilo_vs@hotmail.com>
2022-06-07 17:20:56 -0300

    microsservico para controle de pessoas rodando ok

---
+++ src/test/java/com/projetos/microsservicos/eurekaregistradorservice/EurekaRegistradorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.microsservicos.eurekaregistradorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaRegistradorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
